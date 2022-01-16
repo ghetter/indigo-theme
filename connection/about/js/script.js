c1e07e0454a8e4ee26fe1bd8c48de948666f04e6
ghetter <49289242+ghetter@users.noreply.github.com>
2022-01-16 18:58:19 +0600

    Delete connection directory

--- connection/about/js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('.navigation__icon').click(function(event){
-        $(this).toggleClass('active');
-        $('.navigation__wrapper').toggleClass('active');
-        $('.navigation__list').toggleClass('active');
-        $('body').toggleClass('locked');
-    })
-})
